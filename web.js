7924000bcd66ee181c44dbc44ddc1647d6b6ee9d
Ubuntu <ubuntu@ip-172-31-12-48.us-west-2.compute.internal>
2013-07-10 15:51:24 +0000

    Added loleries

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+var s1= fs.readFileSync("index.html");
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(s1.toString());
 });
 
 var port = process.env.PORT || 5000;
